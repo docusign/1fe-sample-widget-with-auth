24ef681cc10f4f942a6834649a180274120d094d
Sushruth Sastry <2114206+sushruth@users.noreply.github.com>
2025-03-31 13:16:19 -0700

    feat: contracts installation has been implemented (#39)
    * chore: cleaning up unnecessary files
    
    * chore: fix cli package reference in app2
    
    * chore: remove starter-cli since it is obsolete now
    
    * feat: generate types for 1fe-shell to consume everywhere else
    
    * chore: remove vite and use 1fe-cli for all widgets dev and build commands
    
    * feat: [cli] check for widget entry file as well for better error message when it does not exist
    
    * fix: fixing shell test types
    
    * fix: use the correct contract file for contract rollup generation
    
    * feat: contracts install command
    
    * chore: fix props type and remove a desperate comment
    
    * fix: fixed contracts for widgets
    
    * chore: fixing a typo
    
    * chore: removing empty files

---
+++ src/Root.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RootProps } from './contract';
+
+export default function Root(props: RootProps) {
+  return (
+    <>
+      <p>My component from authed widget is mounted!</p>
+    </>
+  );
+}
