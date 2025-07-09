d0cc3d33268a34967f1a19fb4bc8eb13bb2a8c89
Chaitanya Varma <chaitanya.varma@docusign.com>
2025-07-09 15:00:36 -0700

    init

---
+++ .cache/ts-import/Users/chaitanya.varma/repos/1fe-sample-widget-with-auth/.1fe.config.mjs
@@ -0,0 +1,5 @@
+import { getBaseConfig } from '@repo/widget-base-config'; // this is the redistributed package for the organization
+const configuration = {
+    baseConfig: getBaseConfig,
+};
+export default configuration;
