55dbddadedcd23eb2fd921f77602c754627ee4ea
Chaitanya Varma <chaitanya.varma@docusign.com>
2025-07-09 17:06:39 -0700

    remove unneeded folder

--- .cache/ts-import/Users/chaitanya.varma/repos/1fe-sample-widget-with-auth/.1fe.config.mjs
+++
@@ -1,5 +0,0 @@
-import { getBaseConfig } from '@repo/widget-base-config'; // this is the redistributed package for the organization
-const configuration = {
-    baseConfig: getBaseConfig,
-};
-export default configuration;
