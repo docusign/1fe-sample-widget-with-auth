f67a8a9a9136651030680f3066a8c726694e16be
Sushruth Sastry <2114206+sushruth@users.noreply.github.com>
2025-07-23 01:00:19 -0700

    Bundle will be uploaded to: https://1fe-a.akamaihd.net/integration/widgets/@1fe/sample-widget-with-auth/1.0.4/js/1fe-bundle.js / Preview here: https://demo.1fe.com/bathtub?widgetUrl=https://1fe-a.akamaihd.net/integration/widgets/@1fe/sample-widget-with-auth/1.0.4/js/1fe-bundle.js&fixPreview=true [skip ci]

--- src/contract.ts
+++ src/contract.ts
@@ -1,6 +1,9 @@
 import { PlatformPropsType } from "@1fe/shell";
 
-export interface HostPropsContract {}
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface HostPropsContract {
+  // Define the properties that the host can pass to the widget
+}
 
 export type WidgetProps = {
   platform: PlatformPropsType;
