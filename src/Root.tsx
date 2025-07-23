209c579de91349494e08c1a4590abfa6653741fe
Sushruth Sastry <shastry.sushruth@gmail.com>
2025-07-23 00:51:11 -0700

    feat: switch to published 1fe packages and update configs

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { WidgetProps } from "./contract";
+// import { WidgetProps } from "./contract";
 
-export default function Root(props: WidgetProps) {
+export default function Root(/* props: WidgetProps */) {
   return (
     <>
       <p>My component from authed widget is mounted!</p>
